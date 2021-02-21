6330b1ba7b866be55b4c02e2ab7cd6fb9fc94171
shertu <jblackman@deputy.com>
2021-02-21 18:45:13 +1100

    changes to work on mac

--- newstart.ps1
+++ newstart.ps1
@@ -1,14 +1,12 @@
-Set-Location $PSScriptRoot
-./protobuf.ps1
+"$PSScriptRoot/protobuf.ps1"
 
-Set-Location $PSScriptRoot/aspnetapp
+Set-Location "$PSScriptRoot/aspnetapp"
 dotnet build
 
-Set-Location $PSScriptRoot/client
+Set-Location "$PSScriptRoot/client"
 npm install
 
-Set-Location $PSScriptRoot
-./openapi.ps1 "https://localhost:8080"
+"$PSScriptRoot/openapi.ps1 https://localhost:8080"
 
 Set-Location $PSScriptRoot
 docker-compose up -d --build
