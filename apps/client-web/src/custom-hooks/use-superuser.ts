92d85147c79d6029608301d875fcd6e14b9fa026
shertu <djared.xeknau@outlook.com>
2022-06-08 22:38:38 +1000

    Temp

---
+++ apps/client-web/src/custom-hooks/use-superuser.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const superusers: string[] = ['112088567581740211952'];
+
+/**
+ * A custom hook to check the accounts include a superuser.
+ */
+export function useSuperuser(accounts: ApplicationUser[]): boolean {}
